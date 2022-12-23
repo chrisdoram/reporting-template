5644f238a92316e5eb6fe2cec1ddb15d4d3626a0
Chris Oram <chrisoramdev@outlook.com>
2022-12-23 18:22:42 +0000

    added layout shell and os color-scheme support

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
-  return <div>Layout component getting rendered at root</div>
+  return (
+    <div>
+      <header
+        style={{
+          top: 0,
+          position: 'sticky',
+          borderBottom: '1px solid var(--color-neutral-7)',
+          height: '64px',
+        }}
+      >
+        Header
+      </header>
+      <nav
+        style={{
+          position: 'absolute',
+          top: 0,
+          width: '260px',
+          height: '100vh',
+          marginTop: '64px',
+          borderRight: '1px solid var(--color-neutral-7)',
+        }}
+      >
+        Nav
+      </nav>
+      <main
+        style={{
+          marginLeft: '260px',
+        }}
+      >
+        Main
+        <Outlet />
+      </main>
+    </div>
+  )
 }
 
 export default AppLayout
