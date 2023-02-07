a658c8d154b9e76a6fedd9ef8cd90e3db00b7a41
Chris Oram <chrisoramdev@outlook.com>
2023-02-07 13:35:59 +0000

    Added theme toggle and upgraded light/dark theme presets in global css

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { MdDarkMode } from 'react-icons/md'
 
 import styles from './layout.module.css'
 
 const AppLayout = () => {
   return (
     <div className={styles.appShell}>
-      <header className={styles.appShellHeader}>Header</header>
+      <header className={styles.appShellHeader}>
+        <h1>Header</h1>
+        <div
+          className={styles.themeToggleContainer}
+          onClick={() => {
+            const theme = localStorage.getItem('theme')
+            if (theme == 'dark') {
+              localStorage.setItem('theme', 'light')
+              document.body.classList.remove('dark')
+            } else {
+              localStorage.setItem('theme', 'dark')
+              document.body.classList.add('dark')
+            }
+          }}
+        >
+          <MdDarkMode className={styles.themeToggle} />
+        </div>
+      </header>
       <section className={styles.appShellBody}>
         <nav className={styles.appShellNav}>Side Nav</nav>
         <main className={styles.appShellMain}>
