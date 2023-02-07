e6ebc282eac8b3d989341f9dff804f2df3d4d25a
Chris Oram <chrisoramdev@outlook.com>
2023-02-07 17:09:50 +0000

    [WIP] Action buttons added to header

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { MdDarkMode } from 'react-icons/md'
+import { GoLogoGithub, GoLinkExternal } from 'react-icons/go'
+import { RxNotionLogo } from 'react-icons/rx'
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { HiPlus } from 'react-icons/hi'
 
 import styles from './layout.module.css'
 
@@ -8,21 +12,62 @@ const AppLayout = () => {
   return (
     <div className={styles.appShell}>
       <header className={styles.appShellHeader}>
-        <h1>Header</h1>
-        <div
-          className={styles.themeToggleContainer}
-          onClick={() => {
-            const theme = localStorage.getItem('theme')
-            if (theme == 'dark') {
-              localStorage.setItem('theme', 'light')
-              document.body.classList.remove('dark')
-            } else {
-              localStorage.setItem('theme', 'dark')
-              document.body.classList.add('dark')
-            }
-          }}
-        >
-          <MdDarkMode className={styles.themeToggle} />
+        <a href="/" className={styles.mainLogoAnchorContainer}>
+          <RxNotionLogo size={42} className={styles.headerMainLogo} />
+        </a>
+        <button className={styles.recentAction}>
+          <span>Recent</span>
+          <RiArrowDropDownLine size={20} className={styles.dropdownIcon} />
+        </button>
+        <button className={styles.favoriteAction}>
+          <span>Favorites</span>
+          <RiArrowDropDownLine size={20} className={styles.dropdownIcon} />
+        </button>
+        <button className={styles.templateAction}>
+          <span>Templates</span>
+          <RiArrowDropDownLine size={20} className={styles.dropdownIcon} />
+        </button>
+        <button className={styles.createReportAction}>
+          <HiPlus className={styles.plusIcon} />
+        </button>
+        <div className={styles.iconActionsContainer}>
+          <a
+            href="https://github.com/chrisdoram/reporting-template"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.githubAnchorContainer}
+          >
+            <GoLogoGithub className={styles.githubLogoLink} />
+            <GoLinkExternal className={styles.externalLinkLogo} />
+          </a>
+          <div
+            className={styles.themeToggleContainer}
+            onClick={() => {
+              const theme = localStorage.getItem('theme')
+              if (theme == 'dark') {
+                localStorage.setItem('theme', 'light')
+                document.body.classList.remove('dark')
+              } else {
+                localStorage.setItem('theme', 'dark')
+                document.body.classList.add('dark')
+              }
+            }}
+            onKeyDown={(e) => {
+              if (e.code == 'Enter') {
+                const theme = localStorage.getItem('theme')
+                if (theme == 'dark') {
+                  localStorage.setItem('theme', 'light')
+                  document.body.classList.remove('dark')
+                } else {
+                  localStorage.setItem('theme', 'dark')
+                  document.body.classList.add('dark')
+                }
+              }
+            }}
+            tabIndex={0}
+          >
+            <MdDarkMode className={styles.themeToggle} />
+          </div>
         </div>
       </header>
       <section className={styles.appShellBody}>
